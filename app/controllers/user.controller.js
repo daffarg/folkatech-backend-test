fc589d542ed5c0cc05cb7d79e4aeef60fccddb27
Mohamad Daffa Argakoesoemah <argakoesoemahmdaffa@gmail.com>
2023-10-08 00:10:44 +0700

    feat: add update and delete api

--- app/controllers/user.controller.js
+++ app/controllers/user.controller.js
@@ -1,4 +1,3 @@
-const { MongooseError } = require("mongoose");
 const db = require("../models");
 const User = db.users;
 
@@ -94,6 +93,138 @@ exports.findOneByAccountNumber = (req, res) => {
         res.status(500).send({
           message:
             err.message || "Failed to get a user by account number"
+        });
+      });
+  };
+
+// find by account number
+exports.findOneByAccountNumber = (req, res) => {
+    const accountNumber = req.params.accountNumber;
+
+    User.findOne({accountNumber: accountNumber})
+      .then(data => {
+        if (!data) {
+            res.status(404).send({ message: "User not found for the given account number." });
+        } else {
+            res.send(data);
+        }
+      })
+      .catch(err => {
+        res.status(500).send({
+          message:
+            err.message || "Failed to get a user by account number"
+        });
+      });
+  };
+
+// update by identity number
+exports.updateByIdentityNumber = (req, res) => {
+    const identityNumber = req.params.identityNumber
+
+    User.updateOne({identityNumber: identityNumber}, {$set: req.body})
+      .then(result => {
+        if (result.nModified === 0) {
+            res.status(404).send({ message: `User with identity number ${identityNumber} not found`});
+        } else {
+            res.send("User successfully updated");
+        }
+      })
+      .catch(err => {
+        if (err.code === DUPLICATE_KEY_ERR) {
+            res.status(400).send({ message: 'User has already exists' });
+        } else {
+            res.status(500).send({
+            message:
+                err.message || "Failed to update the user"
+            });
+        }
+      });
+  };
+
+// update by account number
+exports.updateByAccountNumber = (req, res) => {
+    const accountNumber = req.params.accountNumber
+
+    User.updateOne({accountNumber: accountNumber}, {$set: req.body})
+      .then(result => {
+        if (result.nModified === 0) {
+            res.status(404).send({ message: `User with account number ${accountNumber} not found`});
+        } else {
+            res.send("User successfully updated");
+        }
+      })
+      .catch(err => {
+        if (err.code === DUPLICATE_KEY_ERR) {
+            res.status(400).send({ message: 'User has already exists' });
+        } else {
+            res.status(500).send({
+            message:
+                err.message || "Failed to update the user"
+            });
+        }
+      });
+  };
+
+// delete all users
+exports.deleteAll = (req, res) => {
+    User.deleteMany({})
+      .then(result => {
+        res.send({
+          message: `${result.deletedCount} users were successfully deleted`
+        });
+      })
+      .catch(err => {
+        res.status(500).send({
+          message:
+            err.message || "Failed to delete all users"
+        });
+      });
+  };
+
+// delete by identity number
+exports.deleteByIdentityNumber = (req, res) => {
+    const identityNumber = req.params.identityNumber;
+  
+    User.deleteOne({identityNumber: identityNumber})
+      .then(result => {
+        if (result.deletedCount === 0) {
+          res.status(404).send({
+            message: `User with identity number ${identityNumber} not found`
+          });
+        } else {
+          res.send({
+            message: "User successfully deleted"
+          });
+        }
+      })
+      .catch(err => {
+        res.status(500).send({
+          message:
+            err.message || "Failed to delete the user"
+        });
+      });
+  };
+
+// delete by account number number
+exports.deleteByAccountNumber = (req, res) => {
+    const accountNumber = req.params.accountNumber;
+  
+    User.deleteOne({accountNumber: accountNumber})
+      .then(result => {
+        if (result.deletedCount === 0) {
+          res.status(404).send({
+            message: `User with account number ${accountNumber} not found`
+          });
+        } else {
+          res.send({
+            message: "User successfully deleted"
+          });
+        }
+      })
+      .catch(err => {
+        res.status(500).send({
+          message:
+            err.message || "Failed to delete the user"
         });
       });
   };
--- app/routes/user.route.js
+++ app/routes/user.route.js
@@ -4,9 +4,17 @@ module.exports = app => {
     var router = require("express").Router();
   
     router.post("/", users.create);
+
     router.get("/", users.findAll);
     router.get("/identityNumber/:identityNumber", users.findOneByIdentityNumber);
     router.get("/accountNumber/:accountNumber", users.findOneByAccountNumber);
+
+    router.put("/identityNumber/:identityNumber", users.updateByIdentityNumber);
+    router.put("/accountNumber/:accountNumber", users.updateByAccountNumber);
+
+    router.delete("/", users.deleteAll);
+    router.delete("/identityNumber/:identityNumber", users.deleteByIdentityNumber);
+    router.delete("/accountNumber/:accountNumber", users.deleteByAccountNumber);
 
     app.use("/api/users", router);
 }
